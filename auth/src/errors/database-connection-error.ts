ca63cd95f40ea85cc42226ef6004cd6e6d7075e2
Rick Hassard <rhassard@gmail.com>
2020-05-20 14:58:25 +0100

    Errors complete

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,4 +1,4 @@
-import { CustomError } from './customer-error';
+import { CustomError } from './custom-error';
 
 export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction} from 'express';
-import { CustomError } from '../errors/customer-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error, 
