6557ecaadb0ca42e208d7bb6c1f815ad08f707af
Rick Hassard <rhassard@gmail.com>
2020-06-01 14:51:55 +0100

    Signup complete

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -33,6 +33,11 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  //Check that environment variable is defined early in the process
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
