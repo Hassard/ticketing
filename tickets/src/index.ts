9759536119c93e8b1cefba000f9a4db6dc6eb5eb
Rick Hassard <rhassard@gmail.com>
2020-07-01 13:53:11 +0100

    End of 304

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
-
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   //Check that environment variable is defined early in the process
@@ -12,6 +12,7 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', '123dsafr5', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
