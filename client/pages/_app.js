00946fbdc61ca5009030a72d8df2f7e04f236b2b
Rick Hassard <rhassard@gmail.com>
2021-01-18 13:47:49 +0000

    Pre @next/codemod next-default-component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,7 +6,7 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <Component currentUser={currentUser} {...pageProps} />
     </div>
   );
 };
@@ -19,7 +19,7 @@ AppComponent.getInitialProps = async (appContext) => {
   // getInitialProps for the page we are trying to show as well
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx, client, data.currentUser);
   }
 
   return {
